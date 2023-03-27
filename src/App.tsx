c66b7d0f5c6a88489bb009acb0d68592973481dc
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-27 19:11:52 +0530

    Add: Custom Theme & Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 export default function App() {
     return (
-        <main className="min-h-screen bg-emerald-500 text-white">
-            <h1 className="text-5xl font-semibold p-4">App</h1>
-        </main>
+        <div className="font-body bg-primary-darkBlue-300 min-h-screen">
+            <main>
+                <h1 className="text-5xl font-semibold p-4 text-accent-red">
+                    App
+                </h1>
+            </main>
+        </div>
     );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    darkBlue: {
+                        100: "hsl(219, 30%, 18%)",
+                        200: "hsl(217, 28%, 15%)",
+                        300: "hsl(218, 28%, 13%)",
+                        700: "hsl(216, 53%, 9%)",
+                    },
+                },
+                accent: {
+                    cyan: "hsl(176, 68%, 64%)",
+                    blue: "hsl(198, 60%, 50%)",
+                    red: "hsl(0, 100%, 63%)",
+                },
+            },
+            fontFamily: {
+                header: "'Raleway', sans-serif",
+                body: "'Open Sans', sans-serif",
+            },
+        },
     },
     plugins: [],
 };
