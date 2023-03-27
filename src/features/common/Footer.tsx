269456120082234dda1d86be8dbc82c866cc002b
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-27 20:00:40 +0530

    Add: Common Components Starter

---
+++ src/features/common/Footer.tsx
@@ -0,0 +1,11 @@
+import WidthRestriction from "@layout/WidthRestriction";
+
+export default function Footer() {
+    return (
+        <footer>
+            <WidthRestriction>
+                <h2>Fylo</h2>
+            </WidthRestriction>
+        </footer>
+    );
+}
---
+++ src/features/common/Header.tsx
@@ -0,0 +1,11 @@
+import WidthRestriction from "@layout/WidthRestriction";
+
+export default function Header() {
+    return (
+        <header>
+            <WidthRestriction>
+                <h1>Fylo</h1>
+            </WidthRestriction>
+        </header>
+    );
+}
