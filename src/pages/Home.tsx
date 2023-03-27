cb0f38a07492a7033d0a347a52ae2e5269b11f9a
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-27 20:00:49 +0530

    Add: Home Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Footer from "@features/common/Footer";
+import Header from "@features/common/Header";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main></main>
+            <Footer />
+        </>
+    );
+}
