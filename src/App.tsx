df33da6092dcad0f79d1181d608fb60ca5bcd49c
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-27 20:01:04 +0530

    Update: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import Home from "@pages/Home";
+
 export default function App() {
     return (
-        <div className="font-body bg-primary-darkBlue-300 min-h-screen">
-            <main>
-                <h1 className="text-5xl font-semibold p-4 text-accent-red">
-                    App
-                </h1>
-            </main>
+        <div className="font-body bg-primary-darkBlue-300 min-h-screen ">
+            <Home />
         </div>
     );
 }
