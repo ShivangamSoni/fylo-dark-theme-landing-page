626673e3bd628f8bdb0b8c346ade0636d59b20e9
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-29 18:24:33 +0530

    Update: Fixed Underline Variant of Link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,15 +1,27 @@
+interface LinkProps
+    extends React.DetailedHTMLProps<
+        React.AnchorHTMLAttributes<HTMLAnchorElement>,
+        HTMLAnchorElement
+    > {
+    variant?: "default" | "fixed";
+}
+
 export default function Link({
     children,
     className,
+    variant = "default",
     ...rest
-}: React.DetailedHTMLProps<
-    React.AnchorHTMLAttributes<HTMLAnchorElement>,
-    HTMLAnchorElement
->) {
+}: LinkProps) {
+    const fixed = variant === "fixed";
+
     return (
         <span>
             <a
-                className={`break-all border-none outline-none [background-image:linear-gradient(to_right,currentColor_0%,currentColor_50%,transparent_50%,transparent_100%)] bg-no-repeat [background-size:300%_0.1em] [background-position:100%_100%] transition-[background-position,color] duration-300 ease-linear hover:[background-position:0%_100%] focus-visible:[background-position:0%_100%] ${className}`}
+                className={`cursor-pointer break-all border-none outline-none [background-image:linear-gradient(to_right,currentColor_0%,currentColor_50%,transparent_50%,transparent_100%)] bg-no-repeat [background-size:300%_0.1em] ${
+                    fixed
+                        ? "[background-position:0%_100%]"
+                        : "[background-position:100%_100%]"
+                } transition-[background-position,color] duration-300 ease-linear hover:[background-position:0%_100%] focus-visible:[background-position:0%_100%] ${className}`}
                 {...rest}
             >
                 {children}
