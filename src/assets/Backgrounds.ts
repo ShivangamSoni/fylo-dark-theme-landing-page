82707f7d9e0556d7d59dde00acbbc81f7c3d5192
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-29 17:33:55 +0530

    Update: Move Backgrounds to Public

--- src/assets/Backgrounds.ts
+++
@@ -1,6 +0,0 @@
-// Backgrounds
-import { ReactComponent as CurveDesktop } from "./bg/curvy-desktop.svg";
-import { ReactComponent as CurveMobile } from "./bg/curvy-mobile.svg";
-import Quotes from "./bg/quotes.png";
-
-export { Quotes, CurveDesktop, CurveMobile };
