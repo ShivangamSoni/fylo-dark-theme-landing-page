d7f8783a563c021fdfa1c141825d02da08a5fa87
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-29 20:21:13 +0530

    Update: Newsletter Validation &  Submit Handler

--- src/features/home/newsletter/index.tsx
+++ src/features/home/newsletter/index.tsx
@@ -1,6 +1,32 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { object, string, InferType } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import Button from "@components/Button";
 
+const FormSchema = object({
+    email: string()
+        .required("Can't be blank")
+        .email("Please enter a valid email address"),
+});
+
+type FormState = InferType<typeof FormSchema>;
+
 export default function NewsLetter() {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<FormState>({
+        resolver: yupResolver(FormSchema),
+    });
+
+    const onSubmit: SubmitHandler<FormState> = (data) => {
+        console.log(data);
+        reset();
+    };
+
     return (
         <div className="bg-primary-darkBlue-200 grid items-start justify-start gap-8 p-12 text-center w-[min(55rem,96%)] mx-auto rounded-lg shadow-lg shadow-black">
             <h2 className="font-bold font-header text-4xl">
@@ -11,16 +37,20 @@ export default function NewsLetter() {
                 extremely generous. If you have any questions, our support team
                 would be happy to help you.
             </p>
-            <form className="grid grid-cols-[1fr_auto] gap-8 items-start text-left">
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="grid grid-cols-[1fr_auto] gap-8 items-start text-left"
+            >
                 <label>
                     <span className="sr-only">Email</span>
                     <input
                         type="email"
                         placeholder="email@example.com"
                         className="w-full rounded-full px-8 py-4 text-xl text-primary-darkBlue-700"
+                        {...register("email")}
                     />
                     <span className="px-8 text-sm text-accent-red font-bold">
-                        Error
+                        {errors.email?.message}
                     </span>
                 </label>
                 <Button>Get Started For Free</Button>
