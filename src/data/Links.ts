84da04d5ae0bb60b53650c0493beb9029ed9ab6c
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-29 16:42:07 +0530

    Add: Nav Links Data

---
+++ src/data/Links.ts
@@ -0,0 +1,17 @@
+export const NavLinks = [
+    {
+        id: crypto.randomUUID(),
+        label: "Features",
+        link: "#",
+    },
+    {
+        id: crypto.randomUUID(),
+        label: "Team",
+        link: "#",
+    },
+    {
+        id: crypto.randomUUID(),
+        label: "Sign In",
+        link: "#",
+    },
+];
