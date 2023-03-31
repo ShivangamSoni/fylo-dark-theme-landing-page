bd7e1b9d0c735c089cb792286622224a0e14eed9
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-31 15:46:30 +0530

    Add: Compressor Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgLoader from "vite-plugin-svgr";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgLoader()],
+    plugins: [
+        react(),
+        svgLoader({
+            exclude: ["/src/assets/logo.svg"],
+        }),
+        viteCompression(),
+    ],
     resolve: {
         alias: [
             {
